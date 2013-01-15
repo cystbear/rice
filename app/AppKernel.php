71594885dd618715f30372efee804cd25ee7c1c5
Oleg Zinchenko <1cdecoder@gmail.com>
2013-01-15 14:29:50 +0200

    added DoctrineFixturesBundle and StofDoctrineExtensionsBundle to kernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,10 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+
             new Rice\DeckKeeperBundle\RiceDeckKeeperBundle(),
         );
 
