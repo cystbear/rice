3e735bb3d14e8b43fbb6495ccc3fe101001d6225
Oleg Zinchenko <1cdecoder@gmail.com>
2013-01-16 03:08:11 +0200

    added addiing CardSet page functionality

--- src/Rice/DeckKeeperBundle/Controller/CardSetController.php
+++ src/Rice/DeckKeeperBundle/Controller/CardSetController.php
@@ -5,6 +5,9 @@ namespace Rice\DeckKeeperBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use Rice\DeckKeeperBundle\Entity\CardSet;
+use Rice\DeckKeeperBundle\Form\CardSetType;
+
 class CardSetController extends Controller
 {
     /**
@@ -22,4 +25,30 @@ class CardSetController extends Controller
             'sets' => $sets,
         );
     }
+
+    public function newAction()
+    {
+        $cardSet = new CardSet();
+        $form = $this->createForm(new CardSetType(), $cardSet);
+
+        $request = $this->getRequest();
+
+        if ('POST' === $request->getMethod()) {
+            $form->bindRequest($request);
+
+            if ($form->isValid()) {
+                $em = $this->getDoctrine()->getEntityManager();
+                $em->persist($cardSet);
+                $em->flush();
+
+                return $this->redirect($this->generateUrl('frontend_deck_keeper_cardset_index'));
+            }
+        }
+
+        return $this->render('RiceDeckKeeperBundle:CardSet:new.html.twig', array(
+            'form' => $form->createView()
+        ));
+
+    }
+
 }
